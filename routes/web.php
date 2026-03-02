bafe91018dd6ca9f346b93c71bb3ea245b0ffb31
Hardiansyah <hardianhardian543@gmail.com>
2026-03-03 00:43:09 +0700

    feat: inisiasi project Laravel – Hardiansyah

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route baru – wajib ditambahkan!
+Route::get('/perkenalan', function () {
+    return '<h1>Halo! Nama saya Hardiansyah</h1>
+            <p>NIM: 4124030 | Prodi: Sistem Informasi</p>
+            <p>Saya siap belajar Laravel! 🚀</p>';
+});
